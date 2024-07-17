e832ba9d66914b312a722b13202c01ceeb46f9c8
Halenina <galenina20032101@gmail.com>
2024-07-18 01:34:59 +0300

    hw4 done

--- src/server.js
+++ src/server.js
@@ -10,25 +10,25 @@ import contactRouter from "./routers/contacts.js"
 const PORT = env("PORT", "3000")
 
 export const setupServer = () => {
-  const app = express();
+    const app = express();
 
-  app.use(express.json());
-  app.use(cors())
+    app.use(express.json());
+    app.use(cors())
 
-  app.use(
-    pino({
-      transport: {
-        target: 'pino-pretty',
-      },
-    }),
-  )
+    app.use(
+        pino({
+            transport: {
+              target: 'pino-pretty',
+            },
+          }),
+    )
 
-  app.use("/contacts", contactRouter);
+    app.use("/contacts", contactRouter);
 
-  app.use(errorHandler)
-  app.use(notFoundHanler)
+    app.use(errorHandler)
+    app.use(notFoundHanler)
 
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-  })
+    app.listen(PORT,() => {
+        console.log(`Server is running on port ${PORT}`)
+    })
 }
---
+++ src/utils/calcPaginationData.js
@@ -0,0 +1,20 @@
+const calcPaginationData = ({total, page, perPage}) => {
+    const totalPages = Math.ceil((total/perPage))
+    const hasNextPages = page < totalPages;
+    const hasPreviousPages = page > 1;
+
+    console.log(page)
+    console.log(perPage)
+    console.log(total)
+    console.log("sfdfadfasfasdf",totalPages)
+    console.log(hasNextPages)
+    console.log(hasPreviousPages)
+
+    return {
+        totalPages,
+        hasNextPages,
+        hasPreviousPages,
+    }
+}
+
+export default calcPaginationData;
