b83fc905317c8c9473d882e87af34e7e03918c34
Halenina <galenina20032101@gmail.com>
2024-08-23 16:42:51 +0300

    Update saveFileToPublicDir.js

--- src/utils/saveFileToPublicDir.js
+++ src/utils/saveFileToPublicDir.js
@@ -7,7 +7,7 @@ const saveFileToPublicDir = async (file, filePath) => {
     const newPath = path.join(PUBLIC_DIR, filePath, file.filename)
   await fs.rename(file.path, newPath);
   
-  return `${env('APP_DOMAIN')}/uploads/${file.filename}`;
+  return `${env('APP_DOMAIN')}/upload/${file.filename}`;
 };
 
 export default saveFileToPublicDir;
