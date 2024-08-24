7a6f9f56556b7819770a96fffb4469e8a9dd89ae
Halenina <galenina20032101@gmail.com>
2024-08-24 13:58:37 +0300

    Update saveFileToPublicDir.js

--- src/utils/saveFileToPublicDir.js
+++ src/utils/saveFileToPublicDir.js
@@ -1,13 +1,13 @@
-import fs from "node:fs/promises"
-import path from "node:path"
+import fs from "node:fs/promises";
+import path from "node:path";
 
 import { PUBLIC_DIR } from "../constants/index.js";
 
 const saveFileToPublicDir = async (file, filePath) => {
-    const newPath = path.join(PUBLIC_DIR, filePath, file.filename)
-  await fs.rename(file.path, newPath);
-  
-  return `${env('APP_DOMAIN')}/upload/${file.filename}`;
+  const newPath = path.join(PUBLIC_DIR, filePath, file.filename);
+    await fs.rename(file.path, newPath);
+
+  return `/${filePath}/${file.filename}`;
 };
 
 export default saveFileToPublicDir;
